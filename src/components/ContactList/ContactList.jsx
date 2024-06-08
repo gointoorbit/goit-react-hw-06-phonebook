4e7e1c408b332d2110b3fb5b209c008aae7e1f4d
gointoorbit <kania.anna@onet.eu>
2024-06-08 20:42:21 +0200

    fixed contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,16 +3,12 @@ import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from '../../redux/selectors';
 import { ContactListItem } from '../ContactListItem/ContactListItem';
 
-const getVisibleContacts = (contacts, filter) => {
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-};
-
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
-  const visibleContacts = getVisibleContacts(contacts, filter);
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <>
